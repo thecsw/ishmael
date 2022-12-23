d2ca7b269573084d942471d13fd2f0b95635887a
Sandy <sandy@sandyuraz.com>
2022-12-23 02:13:36 -0600

    Updated ishmael

---
+++ scripts/time.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+  let time = document.querySelector("#hetime");
+  const day = 24 * 60 * 60 * 1000;
+  cityDuringDayEmojis = ["🌃", "🌃", "🌌", "🌌", "🌉", "🌉",
+                         "🌄", "🌄", "🌅", "🌅", "🏙", "🏙", 
+                         "🏙", "🌁", "🌁", "🌁", "🏞", "🏞",
+                         "🏞", "🌇", "🌇", "🌆", "🌃", "🌃"];
+
+  setTime = () => {
+    let now = new Date();
+    let start = new Date(now.getFullYear(), 0, 0);
+    var diff =
+      now -
+      start +
+      (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
+
+    time.innerText =
+      cityDuringDayEmojis[now.getHours()] +
+      " " +
+      Math.floor(diff / day) +
+      "; " +
+      (now.getFullYear() + 10000) +
+      " H.E. " +
+      (now.getHours() * 100 + now.getMinutes()).toString().padStart(4, "0") +
+      "." +
+      now.getSeconds().toString().padStart(2, "0");
+  };
+
+  setTime();
+  setInterval(setTime, 1000);
+};
